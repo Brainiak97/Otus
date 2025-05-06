866749c044d7cc8e77bc6ef9a0a9b4ab0b3e5622
Vlad Popkov <91893395+Brainiak97@users.noreply.github.com>
2025-05-06 21:27:01 +0300

    Добавьте файлы проекта.

---
+++ TaskHomeWork/FileReader.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+namespace TaskHomeWork;
+
+public static partial class FileReader
+{
+    /// <summary>
+    /// Read three files in directory of app
+    /// </summary>
+    public static async Task<int> GetSpaceFromThreeFilesInAppDirectory()
+    {
+        var directoryPath = AppDomain.CurrentDomain.BaseDirectory;
+        var files = Directory.GetFiles(directoryPath, "*.txt");
+
+        List<Task<int>> tasks = [];
+        foreach (var file in files.Take(3))
+        {
+            tasks.Add(GetSpacesFromFileAsync(file));
+        }
+
+        int[] results = await Task.WhenAll(tasks);
+
+        return results.Sum();
+    }
+
+    /// <summary>
+    /// Read all files in some directory
+    /// </summary>
+    /// <param name="directoryPath"></param>
+    /// <returns></returns>
+    public static async Task<int> GetSpaceFromAllFilesInSomeDirectory(string directoryPath)
+    {
+        var files = Directory.GetFiles(directoryPath, "*.txt");
+
+        List<Task<int>> tasks = [];
+        foreach (var file in files)
+        {
+            tasks.Add(GetSpacesFromFileAsync(file));
+        }
+
+        int[] results = await Task.WhenAll(tasks);
+
+        return results.Sum();
+    }
+
+    private static async Task<int> GetSpacesFromFileAsync(string filePath)
+    {
+        using StreamReader reader = new(filePath);
+        string text = await reader.ReadToEndAsync();
+
+        return SpaceRegex().Count(text);
+    }
+
+    [GeneratedRegex(" ")]
+    private static partial Regex SpaceRegex();
+}
---
+++ TaskHomeWork/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using TaskHomeWork;
+
+Console.WriteLine("Чтение 3 файлов в папке приложения...");
+
+Console.WriteLine("\"Прогрев\" системы...");
+await FileReader.GetSpaceFromThreeFilesInAppDirectory(); // просто чтобы всё скомпилировалось и загрузилось
+Console.WriteLine("\"Прогрев\" завершён.");
+
+Stopwatch stopwatch = new();
+stopwatch.Start();
+var spaceCount = await FileReader.GetSpaceFromThreeFilesInAppDirectory();
+stopwatch.Stop();
+
+Console.WriteLine($"Общее количество пробелов в 3 файлах = {spaceCount}");
+Console.WriteLine($"Затрачено времени (мс): {stopwatch.Elapsed.TotalMilliseconds}");
+
+stopwatch.Reset();
+
+Console.WriteLine("_______________________________________");
+Console.WriteLine("Введите путь к папке для чтения файлов: ");
+var directoryPath = Console.ReadLine();
+Console.WriteLine($"Чтение файлов в папке {directoryPath}...");
+
+if (Directory.Exists(directoryPath))
+{
+    stopwatch.Start();
+    spaceCount = await FileReader.GetSpaceFromAllFilesInSomeDirectory(directoryPath);
+    stopwatch.Stop();
+
+    Console.WriteLine($"Общее количество пробелов в файлах указанной папки = {spaceCount}");
+    Console.WriteLine($"Затрачено времени (мс): {stopwatch.Elapsed.TotalMilliseconds}");
+
+    stopwatch.Reset();
+}
+else
+{
+    Console.WriteLine($"Введенный путь не найден\n{directoryPath}");
+}
